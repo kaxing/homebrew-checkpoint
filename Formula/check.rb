09295e96dd6e08c5b71a6e0a4d4dd8d941e5fffe
kaxing <kaxing@ymail.com>
2026-03-09 18:29:54 +0800

    v0.0.1

--- Formula/check.rb
+++ Formula/check.rb
@@ -6,20 +6,20 @@ class Check < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kaxing/checkpoint/releases/download/v0.0.1/check-macos-arm64.tar.gz"
-      sha256 "ec0345d3aebe056bf6d64c1b1eaceb3d259e4c4fcfd7de0704f0434653e46afc"
+      sha256 "9be2de6ae2dc5502248b66c745eca530eda32dcede0fbe53d0747ab879427ab4"
     else
       url "https://github.com/kaxing/checkpoint/releases/download/v0.0.1/check-macos-x86_64.tar.gz"
-      sha256 "999252155d95b22b2c5ffae50cff59dc0cc4e2b502dd42d8f9967286b0464f6d"
+      sha256 "684bef5b7cf44ab97d4ff3c946d068a293d0775ef4634cff839d0dfdc3ad9316"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kaxing/checkpoint/releases/download/v0.0.1/check-linux-arm64.tar.gz"
-      sha256 "df3b5252f47c6c1fcab085e8bc7559afdc5bd2084eded3bad37839d7fa8e850f"
+      sha256 "77775bf80ce602b9d759b147c11d38cb89e61f1a1f858307f635a445e43f6547"
     else
       url "https://github.com/kaxing/checkpoint/releases/download/v0.0.1/check-linux-x86_64.tar.gz"
-      sha256 "581717e3edc669d460e12eb0d6a214decdc943b9445dc8bfd10a7a3d72b3125b"
+      sha256 "d60cff3730a0bc3dea5efe0bb845b33962972217dbf2eea717b6e8af16fe65e7"
     end
   end
 
